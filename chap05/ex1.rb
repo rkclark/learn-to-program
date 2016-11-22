7e30512b2c61d60c5b83910b1594cb7c1d3a99c3
Rick Clark <rkingsleyclark@gmail.com>
2016-11-22 15:22:01 +0000

    Chap 5

---
+++ chap05/ex1.rb
@@ -0,0 +1,7 @@
+puts "Enter your first name"
+first = gets.chomp!
+puts "Enter your middle name"
+middle = gets.chomp!
+puts "Enter your surname"
+last = gets.chomp!
+puts "Hello #{first} #{middle} #{last}"
---
+++ chap05/ex2.rb
@@ -0,0 +1,3 @@
+puts "Enter your favourite number"
+num = gets.chomp!.to_i
+puts "Your number sucks, #{num + 1} is better"
